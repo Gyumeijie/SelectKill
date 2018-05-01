eb6143eec77478bb56f7319b92087b2529ec4d28
Gyumeijie <18170879819@163.com>
2018-05-01 13:37:42 +0800

    feat: finish the functionality of options scolling

--- key.sh
+++ key.sh
@@ -16,14 +16,19 @@
 #   Return value: selected index (0 for opt1, 1 for opt2 ...)
 
 function select_option {
-
+   
+    local opts=($@) # convert to array
+    local paranum=$#
+    
     # little helpers for terminal print control and key input
+    # TODO put the following functions into a sole file and include
     ESC=$( printf "\033")
     cursor_blink_on()  { printf "$ESC[?25h"; }
     cursor_blink_off() { printf "$ESC[?25l"; }
     cursor_to()        { printf "$ESC[$1;${2:-1}H"; }
-    print_option()     { printf "   $1 "; }
-    print_selected()   { printf "  $ESC[7m $1 $ESC[27m"; }
+    print_option()     { printf "[$ESC[1;91m $1 $ESC[0m]";} 
+    print_marked()   { printf "[$ESC[9m $1 $ESC[29m]"; }
+    print_selected()   { printf "[$ESC[7m $1 $ESC[27m]"; }
     get_cursor_row()   { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
     key_input()        { read -s -n3 key 2>/dev/null >&2
                          if [[ $key = $ESC[A ]]; then echo up;    fi
@@ -31,37 +36,89 @@ function select_option {
                          if [[ $key = ""     ]]; then echo enter; fi; }
 
     # initially print empty new lines (scroll down if at bottom of screen)
-    for opt; do printf "\n"; done
+    #for opt in $opts; do printf "\n"; done
+    for num in {1..5}; do printf "\n"; done
 
     # determine current screen position for overwriting the options
     local lastrow=`get_cursor_row`
-    local startrow=$(($lastrow - $#))
+    # local startrow=$(($lastrow - $#))
+    local startrow=$(($lastrow - 5))
+
+    local selected=0
+
+    function clear_region(){
+      for i in {0..4}; do
+          cursor_to $(($startrow + $i))
+           # clear the line from the current cursor to the end of line
+          printf "$ESC[0K";
+      done
+    }
+
+
+    function update_indexes_down(){
+        local tmp=( "${indexes[@]}" )
+
+        if [ ${indexes[4]} -eq $(($paranum-1)) ]; then
+            indexes=({0..4})
+            return;
+        fi
+
+        for i in {0..4}; do
+           indexes[$i]=$((${tmp[$i]} + 1))
+        done
+
+        selected=4
+    }
+
+     function update_indexes_up(){
+        local tmp=( "${indexes[@]}" )
+
+        if [ ${indexes[0]} == 0 ]; then 
+                  
+           for i in {0..4}; do
+                indexes[$i]=$(($paranum - 5 + $i ))
+           done
+           return;
+        fi
+
+        for i in {0..4}; do
+           indexes[$i]=$((${tmp[$i]} -1 ))
+        done
+
+        selected=0
+    }
+
 
     # ensure cursor and input echoing back on upon a ctrl+c during read -s
     trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
     cursor_blink_off
 
-    local selected=0
     while true; do
         # print options by overwriting the last lines
-        local idx=0
-        for opt; do
-            cursor_to $(($startrow + $idx))
-            if [ $idx -eq $selected ]; then
-                print_selected "$opt"
+        # for opt in $opts; do
+         local idx=0;
+         clear_region
+         for i in {0..4}; do
+            idx=${indexes[$i]}
+            cursor_to $(($startrow + $i))
+            if [ $i -eq $selected ]; then
+                print_selected "${opts[$idx]}" 
             else
-                print_option "$opt"
+                print_option "${opts[$idx]}" 
             fi
-            ((idx++))
+            # ((idx++))
         done
 
         # user key control
         case `key_input` in
-            enter) break;;
+            enter) cursor_to $(($startrow))
+                   break;;
             up)    ((selected--));
-                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+                   # if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+                   if [ $selected -lt 0 ]; then selected=$((5 - 1)); update_indexes_up; fi;;
             down)  ((selected++));
-                   if [ $selected -ge $# ]; then selected=0; fi;;
+                   # if [ $selected -ge $# ]; then selected=0; fi;;
+                   if [ $selected -ge 5 ]; then selected=0; update_indexes_down; fi;;
         esac
     done
 
@@ -77,10 +134,15 @@ function select_option {
 echo "Select one option using up/down keys and enter to confirm:"
 echo
 
-options=("one" "two" "three" "four"  "five" "six" "seven")
+indexes=({0..4})
+options=("one" "two" "three" "four"  "five" "six" "seven" "eight" "nine" "ten" "eleven" "twelve")
 
 select_option "${options[@]}"
 choice=$?
+#deleted=${options[$choice]} 
+#options=( "${options[@]/$deleted}" )
+#select_option "${options[@]}"
 
-echo "Choosen index = $choice"
-echo "        value = ${options[$choice]}"
+index=${indexes[$choice]}
+echo "Choosen index = $index"
+echo "        value = ${options[$index]}"
